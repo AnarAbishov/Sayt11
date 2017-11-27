3f56ce914de8933ff349dc2b0ef19a38149d4951
AnarAbishov <mr.avinar@mail.ru>
2017-11-27 21:41:41 +0400

    Add files via upload

---
+++ Sayt/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sayt.Models;
+namespace Sayt.Controllers
+{
+    public class HomeController : Controller
+    {
+        TaylorEntities db = new TaylorEntities();
+        public ActionResult Index()
+        {
+            ViewBag.Abouts = db.Abouts.First();
+            return View();
+        }
+
+    }
+}
---
+++ Sayt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sayt.MvcApplication" Language="C#" %>
